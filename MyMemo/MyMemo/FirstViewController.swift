9ccaf20257d58db9c957c561869b86d11fa80f01
jinha <jinha@smilegate.com>
2018-08-07 14:22:27 +0900

    프로젝트 생성

---
+++ MyMemo/MyMemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyMemo
+//
+//  Created by user on 2018. 8. 7..
+//  Copyright © 2018년 OgataRina. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
